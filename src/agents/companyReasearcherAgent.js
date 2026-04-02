1e59c135d8491985bc1f0a05f12ea366cd456337
Dibyo Chakraborty <dibyo.dc@gmail.com>
2026-04-02 20:08:27 +0530

    built email writer agent with dedicated prompt , stylePicker util

--- src/agents/companyReasearcherAgent.js
+++ src/agents/companyReasearcherAgent.js
@@ -1,11 +1,12 @@
 import {Agent} from '@openai/agents';
 import {z} from 'zod';
 import {WebSearchTool} from '../agent_tools/webSearch.js';
-import {ResearchPrompt} from '../constants/researchPrompt.js'
+import {ResearchPrompt} from '../constants/prompts/researchPrompt.js'
 
 const outputSchema = z.object({
     category: z.string(),
     technical: z.boolean(),
+    description_wrt_AI: z.string().describe("What does this company do wrt to AI ?"),
     stage: z.enum(["early","mid","large","unknown"]),
     hiring_signal: z.enum(["yes", "no", "unknown"]),
     confidence: z.number().min(0).max(1),
